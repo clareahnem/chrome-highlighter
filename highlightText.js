18b3b03d0db98a17cc6b999cea827703b4ac5ddd
Clare O <clh.oneil@gmail.com>
2024-03-27 17:03:21 +1100

    cleanup highlightText.js

--- highlightText.js
+++ highlightText.js
@@ -1,24 +1,20 @@
-const getSelectedText = () => {
-    let selectedText = ''
-
-    if(document.getSelection) {
-        selectedText = document.getSelection()
-        let range = selectedText.getRangeAt(0).cloneRange()
-
-        const newSpan = document.createElement('span')
-        newSpan.className = 'highlight'
-        range.surroundContents(newSpan)
-        selectedText.removeAllRanges()
-        selectedText.addRange(range)
-        console.log("extractContents", `${selectedText}`)
-    } 
-
-    return selectedText
-}
-
 const highlightSelectedText = (event) => {
     event.preventDefault()
-    getSelectedText()
+    
+    if(document.getSelection) {
+        const selectedText = document.getSelection()
+        let range = selectedText.getRangeAt(0).cloneRange()
+
+        // wrap with span only if there are text highlighted
+        const selectedTextLength = range.endOffset - range.startOffset
+        if(selectedTextLength > 0) {
+            const newSpan = document.createElement('span')
+            newSpan.className = 'highlight'
+            range.surroundContents(newSpan)
+            selectedText.removeAllRanges()
+            selectedText.addRange(range)
+        }
+    }    
 }
 
 document.body.addEventListener('mouseup', highlightSelectedText)
