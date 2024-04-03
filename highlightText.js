d4893bece355465e1f70fc29c4504abddfdcdda5
Clare O <clh.oneil@gmail.com>
2024-04-03 16:19:21 +1100

    Add simple error handling, change method to add highlight colour

--- highlightText.js
+++ highlightText.js
@@ -4,15 +4,23 @@ const highlightSelectedText = (event) => {
     if(document.getSelection) {
         const selectedText = document.getSelection()
         let range = selectedText.getRangeAt(0).cloneRange()
-
-        // wrap with span only if there are text highlighted
-        const selectedTextLength = range.endOffset - range.startOffset
-        if(selectedTextLength > 0) {
-            const newSpan = document.createElement('span')
-            newSpan.className = 'highlight'
-            range.surroundContents(newSpan)
-            selectedText.removeAllRanges()
-            selectedText.addRange(range)
+        
+        try {
+            // wrap with span only if there are text highlighted
+            const selectedTextLength = range.endOffset - range.startOffset
+            const hasNestedElements = range.commonAncestorContainer.childNodes.length > 0 ? true : false
+            const hasTextToHighlight = selectedTextLength > 0 ? true : hasNestedElements
+            if(hasTextToHighlight) {
+                console.log(range)
+                const newSpan = document.createElement('span')
+                newSpan.className = 'highlight'
+                // range.extractContents() removes selected text out of DOM. 
+                // We wrap the extracted content in <span> and then insert it back to DOM
+                newSpan.appendChild(range.extractContents())
+                range.insertNode(newSpan)
+            }
+        } catch(e) {
+            console.error('sorry, we cannot highlight non text values!', e)
         }
     }    
 }
